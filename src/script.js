f214cc8fc1623960958c46b44e5024a6429be0af
Rmaceac <ryan.maceachern.tech@gmail.com>
2023-12-20 14:12:04 -0800

    remove initial errors

--- src/script.js
+++ src/script.js
@@ -101,13 +101,8 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    sphere.rotation.y = 0.1 * elapsedTime
-    cube.rotation.y = 0.1 * elapsedTime
-    torus.rotation.y = 0.1 * elapsedTime
-
-    sphere.rotation.x = 0.15 * elapsedTime
-    cube.rotation.x = 0.15 * elapsedTime
-    torus.rotation.x = 0.15 * elapsedTime
+    // sphere.rotation.y = 0.1 * elapsedTime
+    // sphere.rotation.x = 0.15 * elapsedTime
 
     // Update controls
     controls.update()
